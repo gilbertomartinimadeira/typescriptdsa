7eb7d1bac1d32c41d6515a316fc3f13acec9e22e
GilbertoDroid <gilberto.madeira@outlook.com>
2024-02-24 11:09:02 -0300

    First try on bs for ts

--- src/binarySearch.ts
+++ src/binarySearch.ts
@@ -1,4 +1,32 @@
 export default function binary_search(haystack: number[], needle: number): boolean {
     
+    let split = 0;
+    let lowerEnd = 0;
+    let upperEnd = haystack.length;
+    do {
+    let middle = Math.ceil(lowerEnd + (upperEnd - lowerEnd)/2);
+
+    console.log(`at split ${split} middle is: ${middle}`);
+
+    if(needle === middle) {
+        console.log(`number of split: ${split}`);
+        return true;
+    }
+    else if(needle > middle){
+        lowerEnd = middle + 1;
+        console.log(`number of splits: ${split}`);
+        console.log(`needle is above ${middle}`); 
+    }
+    else {
+        upperEnd = middle;
+
+        console.log(`number of splits: ${split}`);
+        console.log(`needle is below ${middle}`);
+    }
+    split++;
+    }while(lowerEnd < upperEnd);
+    
+    console.log(`final number of splil ts: ${split}`);
+
     return false;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
+const binarySearch = require('./binarySearch').default;
+
 console.log('App is running');
+
+let haystack = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+console.log(haystack);
+
+let needle = 8;
+console.log(`needle: ${needle}`);
+const result = binarySearch(haystack,needle);
+
+console.log(result);
+
+
 
